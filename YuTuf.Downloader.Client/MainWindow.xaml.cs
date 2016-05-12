5b222c5f9553db0dad2df8bc2667d1ad42d4f8bb
maktub82 <gallardo91_@hotmail.com>
2016-05-12 22:10:07 +0200

    dded Solution

---
+++ YuTuf.Downloader.Client/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using WPFFolderBrowser;
+
+namespace YuTuf.Downloader.Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<Video> videos;
+        private string destinationPath = string.Empty;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += OnMainWindowsLoaded;
+        }
+
+        private void OnMainWindowsLoaded(object sender, RoutedEventArgs e)
+        {
+            Initialize();
+        }
+
+        #region Private Methods
+        private void Initialize()
+        {
+            Download.IsEnabled = false;
+            videos = new ObservableCollection<Video>();
+            Videos.ItemsSource = videos;
+        }
+        #endregion
+
+        private void OnSelectFolderClick(object sender, RoutedEventArgs e)
+        {
+
+            WPFFolderBrowserDialog folderDialog = new WPFFolderBrowserDialog("Select destination folder");
+            var result = folderDialog.ShowDialog();
+            if (result.HasValue && result.Value)
+            {
+                destinationPath = folderDialog.FileName;
+                DestinationPath.Text = destinationPath;
+                SelectFolder.IsEnabled = false;
+            }
+
+        }
+    }
+
+    public class Video
+    {
+        public string Title { get; set; }
+        public bool IsDownloaded { get; set; }
+    }
+}
