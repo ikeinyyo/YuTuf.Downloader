1b912a455ce6ea183c3b3aa5d31b4294d5437a65
maktub82 <gallardo91_@hotmail.com>
2016-05-12 23:03:13 +0200

    Download Video

--- YuTuf.Downloader.Client/MainWindow.xaml.cs
+++ YuTuf.Downloader.Client/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using MyToolkit.Multimedia;
 using System;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Diagnostics;
+using System.IO;
+using System.Net;
 using System.Threading.Tasks;
 using System.Windows;
 using WPFFolderBrowser;
@@ -69,23 +72,61 @@ namespace YuTuf.Downloader.Client
             {
                 var id = url.Substring(url.IndexOf("watch?v=")).Replace("watch?v=", "");
                 var thumbnail = YouTube.GetThumbnailUri(id, YouTubeThumbnailSize.Small);
-                var video = await YouTube.GetVideoUriAsync(id, YouTubeQuality.QualityHigh);
+                var youtubeVideo = await YouTube.GetVideoUriAsync(id, YouTubeQuality.QualityHigh);
+                var video = new Video() { Thumbnail = thumbnail.OriginalString, Progress = 0 };
 
                 await Dispatcher.BeginInvoke((Action)(() =>
                 {
-                    videos.Add(new Video() { Thumbnail = thumbnail.OriginalString, IsDownloaded = false });
+                    videos.Add(video);
                 }));
+
+                using (var client = new WebClient())
+                {
+                    client.DownloadProgressChanged += (sender, e) =>
+                    {
+                        video.Progress = e.ProgressPercentage;
+                    };
+                    client.DownloadFileAsync(youtubeVideo.Uri, Path.Combine(destinationPath, "video.mp4"));
+                }
+
             }
             catch (Exception ex)
             {
                 Debug.Write(ex.Message);
             }
         }
+
+        private void Client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
     }
 
-    public class Video
+    public class Video : INotifyPropertyChanged
     {
         public string Thumbnail { get; set; }
-        public bool IsDownloaded { get; set; }
+        private float progress;
+        public float Progress
+        {
+            get
+            {
+                return progress;
+            }
+            set
+            {
+                progress = value;
+                NotifyPropertyChanged("Progress");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string info)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
     }
 }
